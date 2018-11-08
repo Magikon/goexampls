e255d49045efec8832683e960f0038482025b621
mikayel <admin@zzillasoft.com>
2018-11-08 12:34:46 +0400

    first commit

---
+++ keyboard.go
@@ -0,0 +1,24 @@
+/*Package keyboard For parsing keyboard input and convert it to float64 */
+package keyboard
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+//GetFloat function
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
